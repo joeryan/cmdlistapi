7adb88f2a6acdc5d5c47bd6821ebb829e73dfeb6
Joe Ryan <joe@clanryan.us>
2019-08-30 00:44:37 -0400

    added simple string return Get controller

--- Controlers/CommandsController.cs
+++ Controlers/CommandsController.cs
@@ -2,3 +2,18 @@ using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 // using CommandAPI.Models;
 // using Microsoft.EntityFrameworkCore;
+
+namespace cmdapi.Controllers
+{
+    [Route("api/[controller]")]
+    // [ApiController]
+    public class CommandsController : ControllerBase 
+    {
+        // simple GET:
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> GetString()
+        {
+            return new string[] {"hard", "coded", "string"};
+        }
+    }
+}
